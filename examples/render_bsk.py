2ff8046b7c5e6226ae7f5297d006e215c03fa0ae
abdulhady89 <hady.elits@gmail.com>
2025-10-23 18:29:08 +1030

    add render

---
+++ examples/render_bsk.py
@@ -0,0 +1,104 @@
+"""Train an algorithm."""
+import argparse
+import json
+from harl.utils.configs_tools import get_defaults_yaml_args, update_args
+
+
+def main():
+    """Main function."""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--algo",
+        type=str,
+        default="happo",
+        choices=[
+            "happo",
+            "happo_debug",
+            "hatrpo",
+            "haa2c",
+            "haddpg",
+            "hatd3",
+            "hasac",
+            "had3qn",
+            "maddpg",
+            "matd3",
+            "mappo",
+        ],
+        help="Algorithm name. Choose from: happo, hatrpo, haa2c, haddpg, hatd3, hasac, had3qn, maddpg, matd3, mappo.",
+    )
+    parser.add_argument(
+        "--env",
+        type=str,
+        default="pettingzoo_mpe",
+        choices=[
+            "smac",
+            "mamujoco",
+            "pettingzoo_mpe",
+            "gym",
+            "football",
+            "dexhands",
+            "smacv2",
+            "lag",
+            "bsk",
+            "bsk_single_sat",
+            "bsk_het"
+        ],
+        help="Environment name. Choose from: smac, mamujoco, pettingzoo_mpe, gym, football, dexhands, smacv2, lag.",
+    )
+    parser.add_argument(
+        "--exp_name", type=str, default="installtest", help="Experiment name."
+    )
+    parser.add_argument(
+        "--load_config",
+        type=str,
+        default="",
+        help="If set, load existing experiment config file instead of reading from yaml config file.",
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default="/newEra/maxsum/project/CLSHARL/result_collection/single_sat/easy/mappo/seed-00001-2025-10-16-17-58-27/models",
+        help="If set, load model file from this path.",
+    )
+
+    args, unparsed_args = parser.parse_known_args()
+
+    def process(arg):
+        try:
+            return eval(arg)
+        except:
+            return arg
+
+    keys = [k[2:] for k in unparsed_args[0::2]]  # remove -- from argument
+    values = [process(v) for v in unparsed_args[1::2]]
+    unparsed_dict = {k: v for k, v in zip(keys, values)}
+    args = vars(args)  # convert to dict
+    if args["load_config"] != "":  # load config from existing config file
+        with open(args["load_config"], encoding="utf-8") as file:
+            all_config = json.load(file)
+        args["algo"] = all_config["main_args"]["algo"]
+        args["env"] = all_config["main_args"]["env"]
+        algo_args = all_config["algo_args"]
+        env_args = all_config["env_args"]
+    else:  # load config from corresponding yaml file
+        algo_args, env_args = get_defaults_yaml_args(args["algo"], args["env"])
+    # update args from command line
+    update_args(unparsed_dict, algo_args, env_args)
+
+    # Force to activate rendering function
+    algo_args['render']['use_render'] = True
+    algo_args['render']['render_episodes'] = 1
+    algo_args['train']['model_dir'] = args['model_path']
+
+    # start render
+    from harl.runners import RUNNER_REGISTRY
+
+    runner = RUNNER_REGISTRY[args["algo"]](args, algo_args, env_args)
+    runner.run()
+    runner.close()
+
+
+if __name__ == "__main__":
+    main()
